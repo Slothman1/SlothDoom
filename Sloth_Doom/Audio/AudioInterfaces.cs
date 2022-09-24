8d690e58c012cb86fe17e5d276cfc0cc46e5e96c
Andrew <88972912+Slothman1@users.noreply.github.com>
2022-09-25 00:17:40 +1000

    Add project files.

---
+++ Sloth_Doom/Audio/AudioInterfaces.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloth_Doom.Audio
+{
+    public interface IMusic
+    {
+        void StartMusic(Bgm bgm, bool loop);
+
+        public int MaxVolume { get; }
+        public int Volume { get; set; }
+    }
+
+    public interface ISound
+    {
+        //sound controls
+        public void Update();
+        public void Pause();
+        public void Resume();
+        public void Reset();
+
+        //todo: impliment the mobj listeners and sounds
+
+        public int MaxVolume { get; }
+        public int Volume { get; set; }
+
+    }
+}
---
+++ Sloth_Doom/Audio/NullAudio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloth_Doom.Audio
+{
+    public sealed class NullMusic : IMusic
+    {
+        private static NullMusic instance;
+
+        public static NullMusic GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new NullMusic();
+            }
+            return instance;
+        }
+
+        public void StartMusic(Bgm bgm, bool loop) { }
+
+        public int MaxVolume
+        {
+            get { return 15; }
+        }
+
+        public int Volume
+        {
+            get { return 0; }
+            set { }
+        }
+    }
+    //todo: impliment nullSound
+}
